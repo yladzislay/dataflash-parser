e901e548fd0d5a5d695a2ef94d9635f858dd39ac
Uladzislau Darazhei <yladzislay@gmail.com>
2024-02-15 16:21:08 +0300

    [Refactoring]: Move extracting methods from Dataflash to new defined Extractor static class with a ExtractedData class as a result.

--- Dynamic/Dataflash.cs
+++ Dynamic/Dataflash.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using UDIE.Adrupilot.Dataflash.Dynamic.Helpers.Extracting;
 using UDIE.Adrupilot.Dataflash.Dynamic.Helpers.MessagesTransforming;
 
@@ -22,150 +21,8 @@ namespace UDIE.Adrupilot.Dataflash.Dynamic
 
         private void CheckGpsMessagesForBadRecords()
         {
-            if (Messages.TryGetValue("GPS", out var gpsMessages))
-            {
+            if (Messages.TryGetValue("GPS", out var gpsMessages)) 
                 gpsMessages.RemoveAll(message => message.Lat == 0 && message.Lng == 0);
-            }
-        }
-
-        public double GetMaxDropRate()
-        {
-            return (short) Messages["CTUN"].Select(x => x.CRt).Max() / 100.0; // convert from cm/s to m/s.
-        }
-
-        public double GetMaxClimbRate()
-        {
-            return (short) Messages["CTUN"].Select(x => x.CRt).Max() / 100.0; // convert from cm/s to m/s.
-        }
-
-        public float GetMaxGroundSpeed()
-        {
-            return (float) Messages["GPS"].Select(x => x.Spd).Max();
-        }
-
-        public double GetMinGpsAltitude()
-        {
-            return (double) Messages["GPS"].Select(x => x.Alt).Min();
-        }
-
-        public double GetMaxGpsAltitude()
-        {
-            return (double) Messages["GPS"].Select(x => x.Alt).Max();
-        }
-
-        public uint GetTotalVibrationClips()
-        {
-            return Messages["VIBE"].Select(x => x.Clip0).Cast<uint>().Last() +
-                   Messages["VIBE"].Select(x => x.Clip1).Cast<uint>().Last() +
-                   Messages["VIBE"].Select(x => x.Clip2).Cast<uint>().Last();
-        }
-        
-        public double GetGpsPassedFlightDistance()
-        {
-            if (!Messages.ContainsKey("GPS") && !Messages.ContainsKey("GPS[0]")) return 0;
-
-            var key = Messages.ContainsKey("GPS") ? "GPS"
-                : Messages.ContainsKey("GPS[0]") ? "GPS[0]" : string.Empty;
-
-            var gpsLatitudePoints = Messages[key].Select(x => x.Lat).Cast<double>().ToArray();
-            var gpsLongitudePoints = Messages[key].Select(x => x.Lng).Cast<double>().ToArray();
-
-            return CalculationHelper.CalculateGpsTotalPassedDistance(gpsLatitudePoints, gpsLongitudePoints);
-        }
-        
-        public double GetMaxDistanceFromStartPoint()
-        {
-            if (!Messages.ContainsKey("GPS") && !Messages.ContainsKey("GPS[0]")) return 0;
-            var key = Messages.ContainsKey("GPS") ? "GPS"
-                : Messages.ContainsKey("GPS[0]") ? "GPS[0]" : string.Empty;
-
-            var gpsLatitudePoints = Messages[key].Select(x => x.Lat).Cast<double>().ToArray();
-            var gpsLongitudePoints = Messages[key].Select(x => x.Lng).Cast<double>().ToArray();
-
-            return CalculationHelper.CalculateMaxDistanceFromStartPoint(gpsLatitudePoints, gpsLongitudePoints);
-        }
-
-        public dynamic GetGpsDataSetByTimeCode(ulong inputTimeUs)
-        {
-            if (!Messages.ContainsKey("GPS") && !Messages.ContainsKey("GPS[0]")) return null;
-            var key = Messages.ContainsKey("GPS") ? "GPS"
-                : Messages.ContainsKey("GPS[0]") ? "GPS[0]" : string.Empty;
-
-            var result = Messages[key].First(x => (ulong) x.TimeUS == inputTimeUs);
-            return result;
-        }
-
-        public double GetTotalClimb()
-        {
-            IEnumerable<double> altitudes = null;
-            
-            if (Messages.TryGetValue("NKF5", out var nkf5Message))
-            {
-                altitudes = nkf5Message.Select(x => x.HAGL).Cast<double>();
-            }
-            else if (Messages.TryGetValue("GPS", out var gpsMessage))
-            {
-                altitudes = gpsMessage.Select(x => x.Alt).Cast<double>();
-            }
-            
-            return CalculationHelper.CalculateTotalClimb(altitudes);
-        }
-
-        public string ExtractUuid()
-        {
-            var regex = new Regex(@" \w{8} \w{8} \w{8}$");
-            var uuidMessage = (string) Messages["MSG"]
-                .Select(x => x.Message)
-                .FirstOrDefault(y => regex.IsMatch(y));
-
-            return uuidMessage == null ? null : string.Concat(uuidMessage.Split(new[] { ' ' }).Skip(1).ToArray());
-        }
-
-        public string GetFirmware()
-        {
-            var regex = new Regex(@"^(ArduCopter|ArduPlane)");
-            var firmware = (string) Messages["MSG"].Select(x => x.Message).FirstOrDefault(y => regex.IsMatch(y));
-            return firmware;
-        }
-
-        public DateTime? GetFlightStartDate()
-        {
-            if (!Messages.ContainsKey("GPS") && !Messages.ContainsKey("GPS[0]")) return null;
-
-            var key = Messages.ContainsKey("GPS")
-                ? "GPS"
-                : Messages.ContainsKey("GPS[0]")
-                    ? "GPS[0]"
-                    : string.Empty;
-
-            var firstGpsMessage = Messages[key].First();
-
-            var gpsWeek = (ushort)firstGpsMessage.GWk;
-            var gpsSec = (uint)firstGpsMessage.GMS;
-
-            var firstGpsDateTime = ExtractingHelpers.ConvertGpsToUtcDateTime(gpsWeek, gpsSec);
-
-            return firstGpsDateTime;
-        }
-
-        public TimeSpan? GetLoggingDuration()
-        {
-            if (!Messages.ContainsKey("GPS") && !Messages.ContainsKey("GPS[0]")) return null;
-            var key = Messages.ContainsKey("GPS") ? "GPS"
-                : Messages.ContainsKey("GPS[0]") ? "GPS[0]" : string.Empty;
-
-            var firstGpsGwk = (ushort)Messages[key].Select(x => x.GWk).First();
-            var firstGpsGms = (uint)Messages[key].Select(x => x.GMS).First();
-
-            var lastGpsGwk = (ushort)Messages[key].Select(x => x.GWk).Last();
-            var lastGpsGms = (uint)Messages[key].Select(x => x.GMS).Last();
-
-            var startDate = ExtractingHelpers.ConvertGpsToUtcDateTime(firstGpsGwk, firstGpsGms);
-            var lastDate = ExtractingHelpers.ConvertGpsToUtcDateTime(lastGpsGwk, lastGpsGms);
-
-            var result = lastDate.Subtract(startDate);
-
-            return new TimeSpan(result.Days, result.Hours, result.Minutes, result.Seconds);
         }
 
         public IEnumerable<DateTime> GetTimeLine(IEnumerable<ulong> timeline)
