01e70fffd1c5c9125e250222f149d633939de570
Uladzislau Darazhei <yladzislay@gmail.com>
2024-02-15 16:24:38 +0300

    [Refactoring]: Move converting method from Dataflash to new defined Convertor static class. Clean-up Dataflash code.

--- Dynamic/Dataflash.cs
+++ Dynamic/Dataflash.cs
@@ -1,45 +1,24 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using UDIE.Adrupilot.Dataflash.Dynamic.Helpers.Extracting;
 using UDIE.Adrupilot.Dataflash.Dynamic.Helpers.MessagesTransforming;
 
-namespace UDIE.Adrupilot.Dataflash.Dynamic
+namespace UDIE.Adrupilot.Dataflash.Dynamic;
+
+public class Dataflash
 {
-    public class Dataflash
+    public Dictionary<byte, dynamic> FormatMessages { get; }
+    public Dictionary<string, List<dynamic>> Messages { get; }
+
+    public Dataflash(Dictionary<byte, dynamic> formatMessages, Dictionary<string, List<dynamic>> messages)
     {
-        public Dictionary<byte, dynamic> FormatMessages { get; }
-        public Dictionary<string, List<dynamic>> Messages { get; }
+        FormatMessages = formatMessages;
+        Messages = messages;
+        MessagesTransformingHelper.ReGroupByInstances(FormatMessages, Messages);
+        CheckGpsMessagesForBadRecords();
+    }
 
-        public Dataflash(Dictionary<byte, dynamic> formatMessages, Dictionary<string, List<dynamic>> messages)
-        {
-            FormatMessages = formatMessages;
-            Messages = messages;
-            MessagesTransformingHelper.ReGroupByInstances(FormatMessages, Messages);
-            CheckGpsMessagesForBadRecords();
-        }
-
-        private void CheckGpsMessagesForBadRecords()
-        {
-            if (Messages.TryGetValue("GPS", out var gpsMessages)) 
-                gpsMessages.RemoveAll(message => message.Lat == 0 && message.Lng == 0);
-        }
-
-        public IEnumerable<DateTime> GetTimeLine(IEnumerable<ulong> timeline)
-        {
-            if (!Messages.ContainsKey("GPS") && !Messages.ContainsKey("GPS[0]")) return null;
-            var key = Messages.ContainsKey("GPS") ? "GPS"
-                : Messages.ContainsKey("GPS[0]") ? "GPS[0]" : string.Empty;
-
-            var firstGpsMessage = Messages[key].First();
-            var firstGpsTime = ExtractingHelpers.ConvertGpsToUtcDateTime(int.Parse(firstGpsMessage.GWk.ToString()),
-                long.Parse(firstGpsMessage.GMS.ToString()) / 1000.0);
-
-            return timeline
-                .Select(time => ((long)time - (long)(ulong)firstGpsMessage.TimeUS) / 1000)
-                .Select(diffTimeMs => firstGpsTime.AddMilliseconds(diffTimeMs))
-                .Cast<DateTime>()
-                .ToList();
-        }
+    private void CheckGpsMessagesForBadRecords()
+    {
+        if (Messages.TryGetValue("GPS", out var gpsMessages)) 
+            gpsMessages.RemoveAll(message => message.Lat == 0 && message.Lng == 0);
     }
 }
